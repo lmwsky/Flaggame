eadb62e90e6c49e1e4841182dc571ae145b28246
lmw <lmwtclmwtc@163.com>
2016-03-11 14:01:29 +0800

    美化了主界面，加入了单人游戏配置对话框，去掉了一些不必要的事件，整合了一个生死转换的图标

--- app/src/main/java/com/example/isky/flaggame/role/Scout.java
+++ app/src/main/java/com/example/isky/flaggame/role/Scout.java
@@ -1,6 +1,5 @@
 package com.example.isky.flaggame.role;
 
-import com.amap.api.maps2d.model.BitmapDescriptorFactory;
 import com.example.isky.flaggame.game.GameConfig;
 
 /**
@@ -18,14 +17,6 @@ public class Scout extends RoleSign {
         setDist_attract(GameConfig.DIST_ATTRACT_SCOUT);
         setDist_investigate(GameConfig.DIST_INVESTIGATE_SCOUT);
     }
-
-
-    @Override
-    public void setTeam(int team) {
-        super.setTeam(team);
-        setIcon(BitmapDescriptorFactory.fromResource(GameConfig.BITMAP_SCOUT[team % 3]));
-    }
-
     @Override
     public void skill() {
 
